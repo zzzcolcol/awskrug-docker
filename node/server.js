776ea6c454432ed6d765089a157f4b1f7f791382
root <root@ip-172-31-19-162.ap-northeast-2.compute.internal>
2018-07-26 02:17:02 +0000

    change container

--- node/server.js
+++ node/server.js
@@ -1,13 +1,20 @@
+
 'use strict';
+
 const express = require('express');
+
+const os = require('os');
+var hostname = os.hostname();
 
 const PORT = 8000;
 const HOST = '0.0.0.0';
 
 const app = express();
 app.get('/', (req, res) => {
-  res.send('test world!\n');
+  res.send("<html><head><h1>hostname : " + hostname +
+    	"</h1></head><body><img src='https://blog.docker.com/wp-content/uploads/Swarmnado-357x627-30-1.gif'></body></html>");
 });
+
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
 
